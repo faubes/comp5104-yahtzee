79c52e4646705a42dc2520bbc42263d2de8f4899
Joel Faubert <joelfaubert@gmail.com>
2019-09-07 13:16:19 -0400

    Fixing merge conflicts...

--- jf-comp5104-yahtzee/src/main/java/jf/comp5104/yahtzee/Dice.java
+++ jf-comp5104-yahtzee/src/main/java/jf/comp5104/yahtzee/Dice.java
@@ -2,17 +2,10 @@ package jf.comp5104.yahtzee;
 
 public class Dice {
 
-<<<<<<< HEAD
-	public int roll() {
-		return 0;
-	}
-	
-=======
   private int val;
 
   public int roll() {
     return 0;
   }
 
->>>>>>> tmp
 }
--- jf-comp5104-yahtzee/src/test/java/jf/comp5104/yahtzee/DiceTest.java
+++ jf-comp5104-yahtzee/src/test/java/jf/comp5104/yahtzee/DiceTest.java
@@ -12,11 +12,7 @@ public class DiceTest {
 	@Test
 	public void test() {
 		Dice d = new Dice();
-<<<<<<< HEAD
-		AssertEquals(0, d.roll(), "dice without rng always rolls 0");
-=======
 		assertEquals("dice without rng always rolls 0", 0, d.roll());
->>>>>>> tmp
 		// next step: check that a roll returns an integer in [1,6]
 	}
 
