43d316aefbab920635c71d8af945d5f2c9f5043e
Joel Faubert <joelfaubert@gmail.com>
2019-09-13 16:28:59 -0400

    TEST: Defining a Dice class

--- jf-comp5104-yahtzee/src/test/java/jf/comp5104/yahtzee/DiceTest.java
+++ jf-comp5104-yahtzee/src/test/java/jf/comp5104/yahtzee/DiceTest.java
@@ -10,11 +10,41 @@ import static org.junit.Assert.*;
 public class DiceTest {
 
 	@Test
-	public void test() {
+	public void testDiceConstructorWithoutParameters() {
 		Dice d = new Dice();
-		assertEquals("dice without rng always rolls 0", 0, d.roll());
+		assertTrue("New dice automatically rolled", d.getValue >= 1 && d.getValue() <= 6);
 		// next step: check that a roll returns an integer in [1,6]
 	}
+	
+	// want to be able to construct specific dice for testing
+	@Test
+	public void testDiceConstructorWithSpecifiedValue() {
+		Dice d = new Dice(5);
+		assertEquals("Dice has value 5", d.getValue(), 5);
+	}
 
+	// shouldn't create dice in bad state
+	@Test(expected = IllegalStateException.class)
+	public void testDiceConstructorWithBadValue() {
+		Dice d = new Dice(10);
+	}
+	
+	
+	@Test
+	public void testToString() {
+		Dice d = new Dice(1);
+		String expected = "-----\n" +
+						  "| 1 |\n" +
+						  "-----\n";
+		assertEquals("Dice has string representation", d.toString(), expected);
+	}
 
+	@Test
+	public void testRoll() {
+		Dice d = new Dice();
+		d.roll();
+		assertTrue("Dice has non-zero value", d.getValue >= 1 && d.getValue() <= 6);
+	}
+	
+	
 }
